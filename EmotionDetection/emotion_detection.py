679ee9616ed89182c9f46ced3b8437910237ee70
Akash8377 <121550790+Akash8377@users.noreply.github.com>
2026-04-24 01:53:54 +0530

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,29 +1,46 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
+
     if text_to_analyze is None or text_to_analyze.strip() == "":
         return {
-            "error": "Invalid input. Please enter valid text.",
+            "error": "Invalid input",
             "status": 400
         }
 
-    response = {
-        "anger": 0.05,
-        "disgust": 0.02,
-        "fear": 0.1,
-        "joy": 0.75,
-        "sadness": 0.08
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
 
-    dominant_emotion = max(response, key=response.get)
-
-    result = {
-        "anger": response["anger"],
-        "disgust": response["disgust"],
-        "fear": response["fear"],
-        "joy": response["joy"],
-        "sadness": response["sadness"],
-        "dominant_emotion": dominant_emotion
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
     }
 
-    return result
+    response = requests.post(url, json=input_json, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        emotions = data["emotionPredictions"][0]["emotion"]
+
+        dominant_emotion = max(emotions, key=emotions.get)
+
+        return {
+            "anger": emotions["anger"],
+            "disgust": emotions["disgust"],
+            "fear": emotions["fear"],
+            "joy": emotions["joy"],
+            "sadness": emotions["sadness"],
+            "dominant_emotion": dominant_emotion
+        }
+
+    else:
+        return {
+            "error": "API error",
+            "status": response.status_code
+        }
